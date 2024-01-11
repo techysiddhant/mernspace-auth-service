98fcf7c74db16df8ec86d7beb83319c87b712993
techysiddhant <sidjain8000@gmail.com>
2024-01-11 17:28:35 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
 import app from './app';
 import { Config } from './config';
+import logger from './config/logger';
 
 const startServer = () => {
     try {
         app.listen(Config.PORT, () => {
-            // eslint-disable-next-line no-console
-            console.log(`Listing on PORT ${Config.PORT}`);
+            logger.info(`Listing on PORT ${Config.PORT}`);
         });
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error(error);
-        process.exit(1);
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
     }
 };
 startServer();
