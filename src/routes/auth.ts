6c7683610fff66dcea767f144089a530689da79a
techysiddhant <sidjain8000@gmail.com>
2024-01-20 18:11:35 +0530

    add error handling and logger

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,11 +3,12 @@ import { AuthController } from '../controllers/AuthController';
 import { UserService } from '../services/UserService';
 import { AppDataSource } from '../config/data-source';
 import { User } from '../entity/User';
+import logger from '../config/logger';
 const router = express.Router();
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-const authController = new AuthController(userService);
-router.post('/register', (async (req, res) => {
-    await authController.register(req, res);
+const authController = new AuthController(userService, logger);
+router.post('/register', (async (req, res, next) => {
+    await authController.register(req, res, next);
 }) as RequestHandler);
 export default router;
