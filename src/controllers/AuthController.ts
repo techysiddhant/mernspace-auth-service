af7c2c2941a8d016664d20b11603ada0e18d6fe7
techysiddhant <sidjain8000@gmail.com>
2024-01-22 19:15:25 +0530

    refactor tokens

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -4,8 +4,6 @@ import { UserService } from '../services/UserService';
 import { Logger } from 'winston';
 import { validationResult } from 'express-validator';
 import { JwtPayload } from 'jsonwebtoken';
-import { RefreshToken } from '../entity/RefreshToken';
-import { AppDataSource } from '../config/data-source';
 import { TokenService } from '../services/TokenService';
 
 export class AuthController {
@@ -45,14 +43,8 @@ export class AuthController {
                 role: user.role,
             };
             const accessToken = this.tokenService.generateAccessToken(payload);
-            // Persist the Referesh Token
-            const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
-            const refreshTokenRepository =
-                AppDataSource.getRepository(RefreshToken);
-            const newRefreshToken = await refreshTokenRepository.save({
-                user: user,
-                expiresAt: new Date(Date.now() + MS_IN_YEAR),
-            });
+            const newRefreshToken =
+                await this.tokenService.persistRefreshToken(user);
             const refreshToken = this.tokenService.generateRefreshToken({
                 ...payload,
                 id: String(newRefreshToken.id),
