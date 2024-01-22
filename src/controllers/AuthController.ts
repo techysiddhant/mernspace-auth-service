91efd9c7f86d69b3dcff08fcdfff6ed66a668890
techysiddhant <sidjain8000@gmail.com>
2024-01-22 18:45:52 +0530

    persist refreshtoken

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,18 +1,18 @@
-import path from 'path';
-import fs from 'fs';
 import { NextFunction, Response } from 'express';
 import { RegisterUserRequest } from '../types';
 import { UserService } from '../services/UserService';
 import { Logger } from 'winston';
 import { validationResult } from 'express-validator';
-import { JwtPayload, sign } from 'jsonwebtoken';
-import createHttpError from 'http-errors';
-import { Config } from '../config';
+import { JwtPayload } from 'jsonwebtoken';
+import { RefreshToken } from '../entity/RefreshToken';
+import { AppDataSource } from '../config/data-source';
+import { TokenService } from '../services/TokenService';
 
 export class AuthController {
     constructor(
         private userService: UserService,
         private logger: Logger,
+        private tokenService: TokenService,
     ) {}
     async register(
         req: RegisterUserRequest,
@@ -40,33 +40,22 @@ export class AuthController {
                 password,
             });
             this.logger.info('User has been registerd', { id: user.id });
-            let privateKey: Buffer;
-            try {
-                privateKey = fs.readFileSync(
-                    path.join(__dirname, '../../certs/private.pem'),
-                );
-            } catch (err) {
-                const error = createHttpError(
-                    500,
-                    'Error while reading private key!',
-                );
-                next(error);
-                return;
-            }
             const payload: JwtPayload = {
                 sub: String(user.id),
                 role: user.role,
             };
-
-            const accessToken = sign(payload, privateKey, {
-                algorithm: 'RS256',
-                expiresIn: '1h',
-                issuer: 'auth-service',
+            const accessToken = this.tokenService.generateAccessToken(payload);
+            // Persist the Referesh Token
+            const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
+            const refreshTokenRepository =
+                AppDataSource.getRepository(RefreshToken);
+            const newRefreshToken = await refreshTokenRepository.save({
+                user: user,
+                expiresAt: new Date(Date.now() + MS_IN_YEAR),
             });
-            const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
-                algorithm: 'HS256',
-                expiresIn: '1y',
-                issuer: 'auth-service',
+            const refreshToken = this.tokenService.generateRefreshToken({
+                ...payload,
+                id: String(newRefreshToken.id),
             });
             res.cookie('accessToken', accessToken, {
                 domain: 'localhost',
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -5,6 +5,7 @@ import { DataSource } from 'typeorm';
 import { AppDataSource } from '../../src/config/data-source';
 import { Roles } from '../../src/constants';
 import { isJwt } from '../utils';
+import { RefreshToken } from '../../src/entity/RefreshToken';
 // import { truncateTables } from '../utils';
 describe('POST /auth/register', () => {
     let connection: DataSource;
@@ -180,6 +181,31 @@ describe('POST /auth/register', () => {
             expect(refreshToken).not.toBeNull();
             expect(isJwt(accessToken)).toBeTruthy();
             expect(isJwt(refreshToken)).toBeTruthy();
+        });
+
+        it('should store the refresh token in database', async () => {
+            // Arange
+            const userData = {
+                firstName: 'Siddhant',
+                lastName: 'Jain',
+                email: 'sid@test.com',
+                password: 'password',
+            };
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData);
+            //Assert
+            const repository = connection.getRepository(RefreshToken);
+            // const refreshTokens = await repository.find();
+            // expect(refreshTokens).toHaveLength(1);
+            const tokens = await repository
+                .createQueryBuilder('refreshToken')
+                .where('refreshToken.userId = :userId', {
+                    userId: (response.body as Record<string, string>).id,
+                })
+                .getMany();
+            expect(tokens).toHaveLength(1);
         });
     });
     describe('Fields are missing', () => {
